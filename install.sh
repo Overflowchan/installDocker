bb8f0e98110bdd02cf5944a45484a2d5bd07095b
Xazrat <hazratgulamov@gmail.com>
2025-05-29 16:28:44 +0500

    add script for  install Docker

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo "instaling Docker for Ubuntu "
 
 
 
-sudo apt-get update \
-sudo apt-get install ca-certificates curl \
-sudo install -m 0755 -d /etc/apt/keyrings \
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc \
+sudo apt-get update 
+sudo apt-get install ca-certificates curl 
+sudo install -m 0755 -d /etc/apt/keyrings 
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc 
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 
@@ -19,3 +19,5 @@ echo \
 sudo apt-get update
 
 
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
